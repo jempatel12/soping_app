99c3daa2e630e47f0891d453198ef1179928798d
jempatel12 <jemishvavadiya4361@gmail.com>
2023-02-10 18:29:24 +0530

    soping

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+import 'package:soping_app/screens/home_page.dart';
+
+void main() {
+  runApp(
+    const CupertinoApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    ),
+  );
+}
---
+++ lib/screens/global.dart
@@ -0,0 +1,50 @@
+class Global {
+  static List cartList = [];
+  static List<Map> productsList = [
+    {
+      "name": "Apple Watch",
+      "price": "28000",
+      "image": "assets/image/apple watch.jpg",
+    },
+    {
+      "name": "Apple phone",
+      "price": "125000",
+      "image": "assets/image/apple phone.png",
+    },
+    {
+      "name": "Apple Monitor",
+      "price": "19999",
+      "image": "assets/image/apple monitor.png",
+    },
+    {
+      "name": "Apple Headphones",
+      "price": "59900",
+      "image": "assets/images/apple heandphone.png",
+    },
+    {
+      "name": "Apple Smart Speaker",
+      "price": "8499",
+      "image": "assets/image/apple smart speaker.png",
+    },
+    {
+      "name": "Apple Laptop",
+      "price": "2000",
+      "image": "assets/image/laptop.png",
+    },
+    {
+      "name": "Apple TV",
+      "price": "200000",
+      "image": "assets/image/apple tv.png",
+    },
+    {
+      "name": "Apple Home Theater",
+      "price": "28749",
+      "image": "assets/image/apple home theater.png",
+    },
+    {
+      "name": "Apple Watch Charger",
+      "price": "2599",
+      "image": "assets/images/apple wathch charger.png",
+    },
+  ];
+}
